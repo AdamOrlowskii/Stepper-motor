3714c9da25c40d46345e575ed21e6c35b763956a
AdamOrłowskii <aorlowski644@gmail.com>
2025-01-20 23:10:43 +0100

    Dodaj pliki projektów.

---
+++ silnik_krokowy/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace silnik_krokowy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connectButton = new System.Windows.Forms.Button();
+            this.disconnectButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nTextBox = new System.Windows.Forms.TextBox();
+            this.timeTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rightButton = new System.Windows.Forms.Button();
+            this.leftButton = new System.Windows.Forms.Button();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(24, 24);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(216, 45);
+            this.connectButton.TabIndex = 0;
+            this.connectButton.Text = "Połącz urządzenie";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // disconnectButton
+            // 
+            this.disconnectButton.Location = new System.Drawing.Point(270, 24);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(216, 45);
+            this.disconnectButton.TabIndex = 1;
+            this.disconnectButton.Text = "Odłącz urządzenie";
+            this.disconnectButton.UseVisualStyleBackColor = true;
+            this.disconnectButton.Click += new System.EventHandler(this.disconnectButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Sposób sterowania silnikiem:";
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(270, 91);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(216, 28);
+            this.comboBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Liczba kroków:";
+            // 
+            // nTextBox
+            // 
+            this.nTextBox.Location = new System.Drawing.Point(270, 144);
+            this.nTextBox.Name = "nTextBox";
+            this.nTextBox.Size = new System.Drawing.Size(216, 26);
+            this.nTextBox.TabIndex = 5;
+            // 
+            // timeTextBox
+            // 
+            this.timeTextBox.Location = new System.Drawing.Point(270, 196);
+            this.timeTextBox.Name = "timeTextBox";
+            this.timeTextBox.Size = new System.Drawing.Size(216, 26);
+            this.timeTextBox.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 202);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(180, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Czas trwania kroku [ms]:";
+            // 
+            // rightButton
+            // 
+            this.rightButton.Location = new System.Drawing.Point(270, 260);
+            this.rightButton.Name = "rightButton";
+            this.rightButton.Size = new System.Drawing.Size(216, 45);
+            this.rightButton.TabIndex = 9;
+            this.rightButton.Text = "n kroków w prawo";
+            this.rightButton.UseVisualStyleBackColor = true;
+            this.rightButton.Click += new System.EventHandler(this.rightButton_Click);
+            // 
+            // leftButton
+            // 
+            this.leftButton.Location = new System.Drawing.Point(24, 260);
+            this.leftButton.Name = "leftButton";
+            this.leftButton.Size = new System.Drawing.Size(216, 45);
+            this.leftButton.TabIndex = 8;
+            this.leftButton.Text = "n kroków w lewo";
+            this.leftButton.UseVisualStyleBackColor = true;
+            this.leftButton.Click += new System.EventHandler(this.leftButton_Click);
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(24, 331);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(462, 96);
+            this.richTextBox.TabIndex = 10;
+            this.richTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 450);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.rightButton);
+            this.Controls.Add(this.leftButton);
+            this.Controls.Add(this.timeTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.disconnectButton);
+            this.Controls.Add(this.connectButton);
+            this.Name = "Form1";
+            this.Text = "Silnik krokowy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button disconnectButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nTextBox;
+        private System.Windows.Forms.TextBox timeTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button rightButton;
+        private System.Windows.Forms.Button leftButton;
+        private System.Windows.Forms.RichTextBox richTextBox;
+    }
+}
+
---
+++ silnik_krokowy/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FTD2XX_NET;
+
+namespace silnik_krokowy
+{
+    public partial class Form1 : Form
+    {
+        private FTDI silnik = new FTDI();
+
+        private int index4 = 0;
+        private int index8 = 0;
+        private byte[] waveStepSequenceLeft = { 0x04, 0x02, 0x08, 0x01 };                   // sekwencja falowa
+        private byte[] waveStepSequenceRight = { 0x01, 0x08, 0x02, 0x04 };
+        private byte[] fullStepSequenceLeft = { 0x06, 0x0A, 0x09, 0x05 };                   // sekwencja pelnokrokowa
+        private byte[] fullStepSequenceRight = { 0x05, 0x09, 0x0A, 0x06 };
+        private byte[] halfStepSequenceRight = { 0x05, 0x01, 0x09, 0x08, 0x0A, 0x02, 0x06, 0x04 }; // sekwencja polkrokowa
+        private byte[] halfStepSequenceLeft = { 0x04, 0x06, 0x02, 0x0A, 0x08, 0x09, 0x01, 0x05 };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox.Items.Add("falowe");
+            comboBox.Items.Add("pełnokrokowe");
+            comboBox.Items.Add("półkrokowe");
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            FTDI.FT_STATUS ftStatus = silnik.OpenByIndex(0);
+            if (ftStatus == FTDI.FT_STATUS.FT_OK)
+            {
+                richTextBox.AppendText("Polaczono z urzadzeniem.\r\n");
+                silnik.SetBaudRate(9600);
+                silnik.SetBitMode(0xFF, 1); // ustawienie trybu bitowego
+            }
+            else
+            {
+                richTextBox.AppendText("Proba polaczenia zakonczona niepowodzeniem.\r\n");
+            }
+        }
+
+        private void disconnectButton_Click(object sender, EventArgs e)
+        {
+            silnik.SetBitMode(0x00, 0);
+            silnik.Close();
+            richTextBox.AppendText("Odlaczono urzadzenie.\r\n");
+        }
+
+        private void leftButton_Click(object sender, EventArgs e)
+        {
+            MoveMotor(-1);
+        }
+
+        private void rightButton_Click(object sender, EventArgs e)
+        {
+            MoveMotor(1);
+        }
+
+        private void MoveMotor(int direction)
+        {
+            if (!int.TryParse(nTextBox.Text, out int steps) || !int.TryParse(timeTextBox.Text, out int speed))
+            {
+                richTextBox.AppendText("Bledne dane wejsciowe.\r\n");
+                return;
+            }
+
+            string mode = comboBox.SelectedItem?.ToString() ?? "pełnokrokowe";
+
+            switch (mode)
+            {
+                case "pełnokrokowe":
+                    FullStep(steps, direction, speed);
+                    break;
+                case "półkrokowe":
+                    HalfStep(steps, direction, speed);
+                    break;
+                case "falowe":
+                    WaveStep(steps, direction, speed);
+                    break;
+                default:
+                    richTextBox.AppendText("Bledny tryb pracy.\r\n");
+                    break;
+            }
+        }
+
+        private void FullStep(int steps, int direction, int speed)
+        {
+            byte[] sequence = direction > 0 ? fullStepSequenceRight : fullStepSequenceLeft;
+            ExecuteSteps(sequence, steps, speed, 4);
+        }
+
+        private void HalfStep(int steps, int direction, int speed)
+        {
+            byte[] sequence = direction > 0 ? halfStepSequenceRight : halfStepSequenceLeft;
+            ExecuteSteps(sequence, steps, speed, 8);
+        }
+
+        private void WaveStep(int steps, int direction, int speed)
+        {
+            byte[] sequence = direction > 0 ? waveStepSequenceRight : waveStepSequenceLeft;
+            ExecuteSteps(sequence, steps, speed, 4);
+        }
+
+        private void ExecuteSteps(byte[] sequence, int steps, int speed, int length)
+        {
+            for (int i = 0; i < steps; i++)
+            {
+                uint BytesWritten = 0;
+                if (length == 4)
+                {
+                    silnik.Write(new byte[] { sequence[index4] }, 1, ref BytesWritten);
+                    index4++;
+                    index4 %= 4;
+                } else
+                {
+                    silnik.Write(new byte[] { sequence[index8] }, 1, ref BytesWritten);
+                    index8++;
+                    index8 %= 8;
+                }
+                
+                System.Threading.Thread.Sleep(speed);
+            }
+            richTextBox.AppendText($"Wykonano {steps} krokow.\r\n");
+        }
+        private byte[] ReverseArray(byte[] array)
+        {
+            Array.Reverse(array);
+            return array;
+        }
+    }
+}
